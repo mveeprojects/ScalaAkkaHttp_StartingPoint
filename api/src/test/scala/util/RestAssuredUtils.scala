89d498583166d50307058b2c455afd6b03bcd951
MarkVee87 <markvallis1987@Gmail.com>
2020-05-14 13:03:39 +0100

    Added basic tests

---
+++ api/src/test/scala/util/RestAssuredUtils.scala
@@ -0,0 +1,16 @@
+package util
+
+import config.AppConfig
+import io.restassured.RestAssured
+import io.restassured.response.Response
+
+trait RestAssuredUtils extends AppConfig {
+
+  def apiGetRequest: Response = {
+    RestAssured.when().get(s"http://$host:$port")
+  }
+
+  def metricsGetRequest: Response = {
+    RestAssured.when().get(s"http://$host:9095")
+  }
+}
