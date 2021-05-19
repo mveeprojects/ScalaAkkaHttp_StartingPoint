c4e439b237e30d60cc48a351e8c015ec76029baa
mva13 <markvallis1987@Gmail.com>
2021-05-19 12:26:24 +0100

    Added Akka HTTP route with basic auth

--- api/src/main/scala/route/AppRoutes.scala
+++ api/src/main/scala/route/AppRoutes.scala
@@ -4,7 +4,7 @@ import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Directives.{authenticateBasic, complete, concat, get, path, pathSingleSlash}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.Credentials
 import akka.stream.scaladsl.Source
